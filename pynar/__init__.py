732ce5a1abef89b600efd43c1f32cfa9bc5aa190
rnleroux <renan.le-roux@inrae.fr>
2025-04-02 12:48:48 +0200

    test

--- pynar/__init__.py
+++ pynar/__init__.py
@@ -1,3 +1,2 @@
 from .computeIndicator import compute_indicator_netcdf
-from modele_pheno_gdd import proccess_all_year 
-from modele_pheno_gdd import load_parameters 
+from .modele_pheno_gdd import *

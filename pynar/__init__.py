146fc044c6c2b1f50a34d96092a318b7f86fdbda
rnleroux <renan.le-roux@inrae.fr>
2025-04-02 11:49:38 +0200

    ajout fonction load parameters

--- pynar/__init__.py
+++ pynar/__init__.py
@@ -1,2 +1,3 @@
 from .computeIndicator import compute_indicator_netcdf
-from .modele_pheno_gdd import proccess_all_year
+from .modele_pheno_gdd import proccess_all_year 
+from .modele_pheno_gdd import load_parameters 

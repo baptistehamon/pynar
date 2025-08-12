cfa9afd975fbbd11dc3bbbcfe0a9605812967039
Baptiste Hamon <baptiste.hamon@pg.canterbury.ac.nz>
2025-08-12 15:02:53 +1200

    Add required version of xclim dependency

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
         'pandas',  # Bibliothèque pour la manipulation de données
         'xarray',  # Bibliothèque pour le traitement de données multidimensionnelles
         'numpy',   # Bibliothèque pour le calcul numérique
-        'xclim'
+        'xclim >=0.57.1',
     ],
     description='Un package pour calculer des indicateurs climatiques',  # Brève description du package
     author='Renan LE ROUX',  # Nom de l'auteur
